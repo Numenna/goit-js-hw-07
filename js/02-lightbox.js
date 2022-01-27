0737fdb23aee8ce39843da402647ffc264bd2fe7
Nataliia Khintsitska <Natalienumenna@gmail.com>
2022-01-27 23:53:35 +0200

    second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(`.gallery`);
+
+const galleryItemsMarkup = galleryItems.map(({ preview, original, description }) => {
+        return `
+    <div class="gallery__item">
+        <a class="gallery__item" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            />
+        </a>
+    </div>
+    `;
+    })
+    .join(``);
+    
+galleryContainer.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+
+new SimpleLightbox(`.gallery__item a`, {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
